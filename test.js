91b24e6d8b377aeb1f6c7bf48489e1c945c7f852
Tohirjon-Odilov <shahanshoh819@gmail.com>
2023-05-23 01:41:42 +0300

    10 chala

--- test.js
+++ test.js
@@ -208,14 +208,158 @@ function findOutlier(int) {
   // let odd = int.filter((a) => a % 2 !== 0);
   // return even.length == 1 ? even[0] : odd[0];
 
-  // return int.slice(0, 3).reduce((a, b) => (b % 2 === 0 ? a : a + 1), 0) >= 2
-  //   ? int.find((i) => i % 2 === 0)
-  //   : int.find((i) => i % 2 !== 0);
-
-  return int.reduce((a, b) => {
-    console.log(a);
-    console.log(b);
-  });
+  return int.reduce((a, b) => (b % 2 === 0 ? a : a + 1), 0) >= 2
+    ? int.find((i) => i % 2 === 0)
+    : int.find((i) => i % 2 !== 0);
 }
-console.log(findOutlier([2, 4, 0, 100, 4, 11, 2602, 36]));
+// console.log(findOutlier([2, 4, 0, 100, 4, 11, 2602, 36]));
 // console.log(findOutlier([160, 3, 1719, 19, 11, 13, -21]));
+
+//! Edabit 6 masala
+//todo: Sonlar berilgan eng katta va kichik son qaytaruvchi dastur tuzing
+
+function highAndLow(numbers) {
+  //* ishlay olmadim qarab ishladim
+  numbers = numbers.split(" ").sort((a, b) => a - b);
+  return [numbers[numbers.length - 1], numbers[0]].join(" ");
+  //! Best solutions
+  // numbers = numbers.split(" ");
+  // return `${Math.max(...numbers)} ${Math.min(...numbers)}`;
+  //* array ichidagi stringdagi sonlarni har birini Numberga o'tkazish
+  // let arr = numbers.split(" ").map(Number);
+  // return Math.max(...arr) + " " + Math.min(...arr);
+
+  // numbers = numbers.split(" ").sort((a, b) => Number(a) - Number(b));
+  // return [numbers[numbers.length - 1], numbers[0]].join(" ");
+}
+
+// console.log(highAndLow("1 2 30 30 4 5"));
+// console.log(highAndLow("1 1"));
+// console.log(highAndLow("1 9 3 4 -5"));
+
+//! Edabit 7 masala
+//todo: Shaharda harakatlanadigan avtobus bor, u har bir bekatda bir nechta odamni olib, tushiradi. Sizga butun son juftliklari ro'yxati (yoki massiv) taqdim etiladi. Har bir juftlikning elementlari avtobusga chiqqan odamlar sonini (birinchi element) va avtobus bekatida avtobusdan tushgan odamlar sonini (ikkinchi element) ifodalaydi. Sizning vazifangiz - oxirgi avtobus bekatidan keyin (oxirgi massivdan keyin) hali ham avtobusda bo'lgan odamlarning sonini qaytarish. Garchi bu oxirgi avtobus bekati bo'lsa ham, avtobus bo'sh bo'lmasligi mumkin va ba'zi odamlar hali ham avtobus ichida bo'lishi mumkin, ular o'sha erda uxlayotgandir: D Sinov holatlarini ko'rib chiqing.Shuni yodda tutingki, sinov holatlari avtobusdagi odamlar soni har doim >= 0 bo'lishini ta'minlaydi. Shunday qilib, qaytarilgan butun son salbiy bo'lishi mumkin emas. Massivdagi birinchi juftlikdagi ikkinchi qiymat 0 ga teng, chunki birinchi avtobus bekatida avtobus bo'sh.
+
+var number = function (busStops) {
+  // let end = 0,
+  //   kel = 0,
+  //   ket = 0;
+  // return busStops
+  //   .map((a) => {
+  //     return a.reduce((a, b) => {
+  //       kel += a;
+  //       ket += b;
+  //       end = kel - ket;
+  //       return end;
+  //     });
+  //   })
+  //   .pop();
+
+  //! Best solutions
+  // return busStops.reduce((ref, [on, off]) => ref + on - off, 0);
+  //? agar oxiridagi no'l bo'lmasa NaN qaytadi
+  return busStops.reduce((a, b) => a + b[0] - b[1], 0);
+
+  // let allPeople = 0;
+  // busStops.forEach((a) => {
+  // allPeople += a[0];
+  // allPeople -= a[1];
+  // });
+  // return allPeople;
+};
+// console.log(number([[10, 0],[30, 5],[1, 15],],5));
+// console.log(number(([[3,0],[9,1],[4,10],[12,2],[6,1],[7,10]]),17))
+// console.log(number(([[3,0],[9,1],[4,8],[12,2],[6,1],[7,8]]),21))
+// console.log(number(([[0,0]]),0))
+
+//! Edabit 8 masala
+//todo: Satrlar berilgan satr ichidagi eng kichik satrning uzunligini qaytaradigan dastur tuzilsin
+
+function findShort(s) {
+  // let arr = [];
+  // s.split(" ").map((a) => arr.push(a.length));
+  // return Math.min(...arr);
+  //! Best solutions
+  // return Math.min.apply(
+  // null,
+  // s.split(" ").map((w) => w.length)
+  // );
+  return Math.min(...s.split(" ").map((a) => a.length)); //! Keyin o'ylab topdim
+  // return s.split(' ').sort((a,b)=>a.length - b.length).shift().length
+}
+
+// console.log(findShort("bitcoin over world maybe knows perhaps")); // 3
+// console.log(findShort("turns out random test cases are easier than writing out basic ones")); //3
+// console.log(findShort("Let's travel abroad shall we")); //2
+
+//! Edabit 9 masala
+//todo: 10 lik sanoq sistemasini 2 likka o'tkazamiz va undagi 1 larni qo'shib natijani chiqaramiz
+
+var countBits = function (n) {
+  //? ishlay olmadim
+  return n.toString(2).split("0").join("").length;
+  // return n.toString(2).replace(/0/g, "").length;
+  for (c = 0; n; n >>= 1) c += n & 1;
+  return c;
+};
+console.log(countBits(10));
+//! Edabit 10 masala
+//todo:
+
+function high(x) {
+  const alphabet = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  const differense = [];
+  let m = 0;
+  let v = [];
+  x.split(" ").forEach((stri) => {
+    stri = stri.split("");
+    m = 0;
+    stri.forEach((a) => {
+      alphabet.find((i, id) => {
+        id = id + 1;
+        if (i.toLowerCase() === a) {
+          m += id;
+        }
+      });
+      m;
+    });
+    differense.push(m);
+  });
+  return differense;
+}
+
+// console.log(high("man i need a taxi up to ubud")); //"taxi"
+// console.log(high("what time are we climbing up the volcano")); //"volcano"
+// console.log(high("take me to semynak")); //"semynak"
+console.log(high("no sa salmz")); //"aa"
+// console.log(high("c aa")); //"b"
+// console.log(high("bb d")); //"bb"
+// console.log(high("d bb")); //"d"
+// console.log(high("adq")); //"aaa"
